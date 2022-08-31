97dac36fb426b8004424090f6195a339bc785386
Leonardo T. S <75705634+ShinKraft@users.noreply.github.com>
2022-08-31 14:24:16 -0300

    Created About Page base.

---
+++ pages/About.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+type AboutProps = {};
+
+const About: NextPage<AboutProps> = () => {
+	return (
+		<main className="">
+			<h1>This is About page!</h1>
+		</main>
+	);
+};
+
+export default About;
