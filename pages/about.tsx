086cd5f679d86947cf86fb7ead256e5f4a013d65
VitorPrates <111801858+VitorPrates@users.noreply.github.com>
2022-09-05 20:15:17 -0300

    criei a about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import defautstilo from "../styles/About.module.css"
+
+const About : NextPage = () => {
+    return (
+        <div className={defautstilo.perfil}>
+            <div className={defautstilo.rodelaimg}>
+                <p></p>
+            </div>
+        </div>
+    )
+        
+} 
+
+export default About;
